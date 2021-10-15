cf92b8501b01782a766dc57fbb68d6e78bdca8a5
James Reed <reedandj1@gmail.com>
2021-10-15 23:44:52 +0100

    added header and footer components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright © 2021 reed756</p>
+        </footer>
+    )
+}
+
+export default Footer
